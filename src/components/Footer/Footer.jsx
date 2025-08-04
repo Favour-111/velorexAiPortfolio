3b8781d60d7121d52fbb173a2a2b6795db0ed03b
favour <omojolaobaloluwa@gmail.com>
2025-08-04 15:38:18 +0100

    done

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,16 @@
 import React from "react";
 import "./Footer.css";
+import { GiArtificialHive } from "react-icons/gi";
+import { Link } from "react-router";
+import {
+  FaDiscord,
+  FaFacebook,
+  FaInstagram,
+  FaTiktok,
+  FaTwitter,
+} from "react-icons/fa6";
+import { FaFacebookF } from "react-icons/fa";
+import { BsTwitterX } from "react-icons/bs";
 const Footer = () => {
   return (
     <div>
@@ -16,7 +27,78 @@ const Footer = () => {
             <button>Contact us</button>
           </div>
         </div>
-        <div className="footer-bottom">d</div>
+        <div className="footer-bottom">
+          <div className="footer-bottom-container">
+            <div className="footer-itm">
+              <div className="Footer-Logo">
+                <div className="logo">
+                  <GiArtificialHive className="mb-1" />
+                </div>
+                <div>Velorex</div>
+              </div>
+            </div>
+
+            <div className="footer-itm">
+              <div className="footer-itm-head">Contact</div>
+              <div className="address-itm">
+                <div className="address">
+                  PO Box 16122 Collins Street West Victoria 8007 Australia
+                </div>
+                <div>geral@example.com</div>
+                <div>(+23) 345 322 233</div>
+              </div>
+            </div>
+            <div className="footer-itm">
+              <div className="footer-itm-head">Quick Link</div>
+              <ul>
+                <li>
+                  <Link className="footer-link-itm">Home</Link>
+                </li>
+                <li>
+                  <Link className="footer-link-itm">About</Link>
+                </li>
+                <li>
+                  <Link className="footer-link-itm">Contact</Link>
+                </li>
+                <li>
+                  <Link className="footer-link-itm">Gallery</Link>
+                </li>
+              </ul>
+            </div>
+            <div className="footer-itm">
+              <div className="footer-itm-head">Legal and Press</div>
+              <ul>
+                <li>
+                  <Link className="footer-link-itm">Privacy Policy</Link>
+                </li>
+                <li>
+                  <Link className="footer-link-itm">Terms & Condition</Link>
+                </li>
+                <li>
+                  <Link className="footer-link-itm">PressKit</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="footer-social-container">
+            <div className="footer-social-item">
+              <FaInstagram />
+            </div>
+            <div className="footer-social-item">
+              <FaFacebookF />
+            </div>
+            <div className="footer-social-item">
+              <BsTwitterX />
+            </div>
+            <div className="footer-social-item">
+              <FaTiktok />
+            </div>
+            <div className="footer-social-item">
+              <FaDiscord />
+            </div>
+          </div>
+          <div className="copyright">2025 © Pine Horbah's tech</div>
+        </div>
       </div>
     </div>
   );
